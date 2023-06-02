12ec6b45d7b34db6c108e6759aa744c8e508cb30
maianh202 <23a4040005@hvnh.edu.vn>
2023-06-03 02:49:15 +0700

    Update capnhattrangthaidanggiaothuchien.php
    capnhatnhanvien

--- admin/capnhattrangthaidanggiaothuchien.php
+++ admin/capnhattrangthaidanggiaothuchien.php
@@ -6,6 +6,7 @@
      $ketnoi = mysqli_connect($host, $dbusername, $dbpassword, $dbname);
      mysqli_set_charset($ketnoi, 'UTF8');
     $id = $_GET["id"];
+
 	$sql1 = "
     SELECT * 
     FROM donhang 
@@ -14,13 +15,18 @@
 	$dulieu = mysqli_query($ketnoi, $sql1);
 	$row = mysqli_fetch_array($dulieu); 
 
-
+	$sql3 = "
+	SELECT * 
+	FROM nhanvien";
+  $dulieu3 = mysqli_query($ketnoi, $sql3);
+  $row3 = mysqli_fetch_array($dulieu3); 
+  $idnv= $row3["nhanvienid"];
 	
 	if (number_format($row["trangthaiid"])==1)
 
 	{
 		$sql=" 
-		 UPDATE `donhang` SET `trangthaiid` = '2' and  where donhangid='".$id."';";
+		 UPDATE `donhang` SET `trangthaiid` = '2', `nhanvienid`='".$idnv."' where donhangid='".$id."';";
 
 
 	// Cho thực thi câu lệnh SQL trên
@@ -34,7 +40,7 @@
 	}
 	else  
 		$sql=" 
-		 UPDATE `donhang` SET `trangthaiid` = '4' where donhangid='".$id."';";
+		 UPDATE `donhang` SET `trangthaiid` = '4' and ngaynhan='current_timestamp()' where donhangid='".$id."';";
 
 	mysqli_query($ketnoi, $sql);
 	
